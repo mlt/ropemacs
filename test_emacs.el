b86121a75378aa96943a888250e9fa9635d78875
Mikhail Titov <mlt@gmx.us>
2024-08-20 12:34:42 -0500

    Test rope-find-occurences

--- test_emacs.el
+++ test_emacs.el
@@ -1,3 +1,4 @@
+;; This is the only sane way to capture coverage data on Windows
 (add-hook 'kill-emacs-hook (lambda () (pymacs-exec "cov.save()")))
 
 (require 'ert-x)
@@ -11,29 +12,25 @@
     (make-directory dir t)))
 (rope-open-project ".")
 
-(defconst pycode "class SomeClass:
-  \"\"\"docstring\"\"\"
-  def abc(self):
-      pass
-")
-
 (defconst pydoc "*rope-pydoc*")
+
+(defconst b (find-file "ropemacs/__init__.py"))
 
 (ert-deftest show-doc-none ()
   "Test that we can get info that no Python docs available"
-  (ert-with-message-capture captured
-    (with-temp-buffer
-      (insert pycode)
-      (goto-char (point-min))
-      (rope-show-doc "SomeClass"))
-    (should (string-prefix-p "No docs available!" captured)))
+  (ert-with-message-capture
+   captured
+   (with-current-buffer b
+     (goto-char (point-min))
+     (rope-show-doc nil))
+   (should (string-prefix-p "No docs available!" captured)))
   (should-not (get-buffer pydoc)))
 
 (ert-deftest show-doc ()
   "Test that we can get Python docs"
-  (with-temp-buffer
-    (insert pycode)
-    (search-backward "Some")
+  (with-current-buffer b
+    (goto-char (point-min))
+    (search-forward "_make_b")
     (rope-show-doc nil)
     (let ((b (get-buffer pydoc)))
       (should b)
@@ -41,5 +38,16 @@
         (should buffer-read-only)
         (save-excursion
           (goto-char (point-min))
-          (should (search-forward "docstring" nil t))))))
+          (should (search-forward "Make an emacs buffer" nil t))))))
   (kill-buffer pydoc))
+
+(ert-deftest show-occurences ()
+  "Test whether can navigate occurences"
+  (with-current-buffer b
+    (goto-char (point-min))
+    (search-forward "_make_buffer")
+    (cl-letf (((symbol-function ropemacs-completing-read-function)
+               (lambda (&rest _) "search")))
+      (rope-find-occurrences))
+    (dotimes (i 5)               ; only 4 occurences, should wrap around
+      (should (next-error)))))
